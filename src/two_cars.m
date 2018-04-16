ae1fc705d15385e7e8a303e591a2c1940f7f3541
jay ess <mentoce@gmail.com>
2018-04-15 18:08:42 -0700

    Simulation for a two-vehicle platoon

--- src/two_cars.m
+++ src/two_cars.m
@@ -1,29 +1,62 @@
-% Start with two human drivers
-%
-alpha = 1; beta = 1;
-v_max = 15; h_stop = 5; h_go = 25;
+clear all
+alpha1 = 0.6; beta1 = 0.9;
+alpha2 = 0.9; beta2 = 0.6;
+v_max = 25; h_stop = 5; h_go = 30;
 
-hv1 = HumanDriver(v_max, h_stop, h_go, alpha, beta);
-hv2 = HumanDriver(v_max, h_stop, h_go, alpha, beta);
+A = [
+0,      -1,               1;
+0, -(alpha1 + beta1),     beta1;
+0,      beta2,       -(alpha2 + beta2)];
 
-run_time = 30; % seconds
-delta_t = 0.001; % 1 ms
-num_its =  run_time/delta_t + 1;
-X0 = [30; 0; 0];
-X = zeros(3, num_its);
-X_dot = X;
-X(:, 1) = X0;
+B = [
+0 ,0;
+beta1, 0;
+0, beta2
+];
 
-vel1 = 0; % velocity of hv1
-vel2 = 0; % velocity of hv2
+C = [1, 0, 0];
 
-for i = 1:delta_t:run_time+1
-  i
-  notice = strcat('Iteration: ', num2str(i));
-  vel1 = vel1 + X(2, i) * delta_t
-  vel2 = vel2 + X(3, i) * delta_t
-  h_dot = X(3, i) - X(2, i)
-  X_dot(1, i+1) = X_dot(1, i) + h_dot * delta_t;
-  X_dot(2, i+1) = X_dot(2, i) + hv1.v_dot(vel2) * delta_t;
-  X_dot(3, i+1) = X_dot(2, i) + hv2.v_dot(vel1) * delta_t;
+D = [0, 0];
+
+X = [27, 30, 75]';
+
+t=0;%simulation starting time
+dt=0.01;%step size
+tsim=50.0;%finish time
+n=round((tsim-t)/dt); %no. of iterations
+
+for i=1:n;
+  u1 = range_policy(X(1), h_stop, h_go, v_max);
+  u2 = range_policy(-X(1), h_stop, h_go, v_max);
+  U = [u1; u2];
+  % u=4;%fixed input
+  dx=A*X+B*U;
+  X=X+dx*dt;
+  Y=C*X;
+  Y1(i,:)=[t, Y];
+  X1(i,:)=[t, X'];
+  U1(i,:)=[t, U'];
+  t=t+dt;
 end
+
+subplot(3,1,1)
+plot(X1(:,1),X1(:,2:3),'linewidth', 2)
+% axis([0 10 -10 10])
+xlabel('time')
+ylabel('v(t)')
+title('Velocity of vehicles')
+legend('v_{1}', 'v_{2}');
+subplot(3,1,2)
+plot(Y1(:,1),Y1(:,2),'linewidth', 2);
+% axis([0 10 -10 10])
+xlabel('time')
+ylabel('h(t)')
+title('Headway of vehicles')
+legend('h_{1}');
+subplot(3,1,3)
+plot(U1(:,1),U1(:,2:3),'linewidth', 2);
+% axis([0 10 -10 10])
+xlabel('time')
+ylabel('V(h(t))')
+title('Range policy')
+legend('V(h_{1})', 'V(h_{2})');
