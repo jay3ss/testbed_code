02378f0f3a111243431ca6b8d348b5eece906645
josh <mentoce@gmail.com>
2018-04-26 14:01:18 -0700

    Works in Matlab, need to fix issue with cars being populated in negative positions on road

--- src/Road.m
+++ src/Road.m
@@ -2,39 +2,67 @@
 % of this has been borrowed from/inspired by
 % https://github.com/movsim/traffic-simulation-de
 
-classdef Road
+classdef Road < handle
   properties (Access=public)
+    road_length
+    num_vehicles
+    vehicles
   end
   properties (Access=private)
-  % @param road_length_:    link length [m]
+  % @param road_length:    link length [m]
   % @param traj_x_:         function arc length u -> phys x coordinate (East)
   % @param traj_y_:         function arc length u -> phys y coordinate (North)
   % @param speed_init_:     initial longitudinal speed [m/s]
-  % @param vehicles_:       array of vehicles on road
-    road_length_
+  % @param vehicles:       array of vehicles on road
+    
     traj_x_
     traj_y_
     speed_init_
-    vehicles_
   end
-  methods (Access=public)
-    function obj = Road(road_length, init_density, init_speed)
-      obj.road_length_ = road_length;
-      obj.vehicles_ = obj.createVehicleArray(init_density, init_speed);
+  methods
+    function obj = Road(road_length, init_density, vehicle, v_def)
+      % Constructor
+      % @param road_length = length of the road
+      % @param init_density = intial density of the road [vehicle/m]
+      % @param vehicle: vehicle object
+      % @param v_def:   container.Map that contains init_speed and length
+      % keys
+      obj.road_length = road_length;
+      obj.num_vehicles = floor(obj.road_length * init_density);
+      obj.vehicles = obj.createVehicleArray(vehicle, v_def);
+    end
+    
+    function num_vehicles = get.num_vehicles(obj)
+      num_vehicles = obj.num_vehicles;
+    end
+    
+    function road_length = get.road_length(obj)
+      road_length = obj.road_length;
+    end
+    
+    function vels = get.vehicles(obj)
+      vels = obj.vehicles;
     end
   end
   methods (Access=private)
-    function [vehicles] = createVehicleArray(obj, init_density, init_speed)
-      vel_length = 7; % [m]
-      speed = 0.8 * init_speed;
-      num_vehicles = floor(obj.road_length_ * init_density);
-      obj.vehicles_(1, num_vehicles) = Vehicle(vel_length);
+    function [vehicles] = createVehicleArray(obj, vehicle, v_def)
+      % Creates an array of homogeneous vehicles
+      % @param vehicle: vehicle object
+      % @param v_def:   container.Map that contains init_speed and length
+      % keys
+      % @returns vehicles: array of homogeneous vehicles
+      speed = 0.8 * v_def('init_speed');
+      vel_len = v_def('length');
+      n_vehicles = obj.num_vehicles;
+      vehicles(1, n_vehicles) = vehicle;
+      % obj.vehicles(1, num_vehicles) = Vehicle(vel_length);
 
-      for i = num_vehicles:-1:1 % counting backwards for Memory pre-allocation
+      for i = n_vehicles:-1:1 % counting backwards for Memory pre-allocation
         % find the position of the current vehicle
-        u = (num_vehicles-i-1) * obj.road_length_/(num_vehicles);
-        obj.vehicles_(i).setu(u);
-        obj.vehicles_(i).setspeed(speed);
+        u = (n_vehicles-i-1) * obj.road_length/(n_vehicles);
+        vehicles(i).u = u;
+        vehicles(i).length = vel_len;
+        vehicles(i).speed = speed;
       end
     end
   end
--- src/Vehicle.m
+++ src/Vehicle.m
@@ -3,39 +3,51 @@
 % https://github.com/movsim/traffic-simulation-de
 
 classdef Vehicle < handle
-  properties (Access=public)
-  end
-  properties (Access=private)
-  % length_: length of the vehicle [m]
+%   properties (Access=public)
+%   end
+  properties %(Access=private)
+  % length: length of the vehicle [m]
   % u:       longitudinal coordinate (arc-length) [m]
   % speed:   speed of vehicle [m/s]
-    length_
+    length
     u
     speed
   end
 
-  methods (Access=public)
-    function obj = Vehicle(vel_length)
-      % if nargin == 3
-      %   obj.length_ = vel_length;
-      %   obj.u = u;
-      %   obj.speed_ = speed;
-      % end
-      obj.length_ = vel_length;
-      obj.u = u;
-      obj.speed = speed;
+  methods
+    function obj = Vehicle(vel_length, u)
+      if nargin == 3
+        obj.length = vel_length;
+        obj.u = u;
+        obj.speed = speed;
+      end
+      % obj.length = vel_length;
+      % obj.u = u;
+      % obj.speed = speed;
     end
 
     function set.u(obj, u)
+      obj.u = u;
     end
 
     function u = get.u(obj)
+        u = obj.u;
     end
 
-    function set.speed(obj, u)
+    function set.speed(obj, s)
+      obj.speed = s;
     end
 
     function speed = get.speed(obj)
+        speed = obj.speed;
+    end
+    
+    function set.length(obj, l)
+      obj.length = l;
+    end
+
+    function l = get.length(obj)
+      l = obj.length;
     end
   end
   methods (Access=private)
