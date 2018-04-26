77fad8c1d0121e99ef71c2ec5018b764841ab014
jay ess <mentoce@gmail.com>
2018-04-25 21:26:03 -0700

    Add headway validation

--- src/five_cars.m
+++ src/five_cars.m
@@ -1,11 +1,11 @@
 clear all; close all;
 
-v_max = 35; h_stop = 5; h_go = 15;
+v_max = 35; h_stop = 5; h_go = 30;
 
 alpha1 = 0.6; beta1 = 0.9;
-alpha2 = 0.6; beta2 = 0.9;
+alpha2 = 0.9; beta2 = 0.6;
 alpha3 = 0.6; beta3 = 0.9;
-alpha4 = 0.6; beta4 = 0.9;
+alpha4 = 0.9; beta4 = 0.6;
 alpha5 = 0.6; beta5 = 0.9;
 
 ab1 = -(alpha1 + beta1);
@@ -46,21 +46,27 @@ tsim=50.0;%finish time
 n=round((tsim-t)/dt); %no. of iterations
 
 % Set the initial conditions
-%    h1,  v1, h2,  v2, h3, v3, h4, v4, h5,  v5
+%   [h1, v1, h2, v2, h3, v3, h4, v4, h5, v5]
 X = [40, 50, 40, 50, 40, 20, 40, 20, 25, 20]';
+prev_h1 = 0; h1 = X(1);
+prev_h2 = 0; h2 = X(3);
+prev_h3 = 0; h3 = X(5);
+prev_h4 = 0; h4 = X(7);
+prev_h5 = 0; h5 = X(9);
 
+% Basic premise
+% - find the control action (range policy)
+% - update the velocities/headways of each vehicle
+% - check the headway/velocity of each vehicle for collisions
+% - if there's a collision then recalculate the headway & velocity
 for i=1:n;
-  % Find the headways
-  h1 = X(10) - X(2);
-  h2 = X(2) - X(4);
-  h3 = X(4) - X(6);
-  h4 = X(6) - X(8);
-  h5 = X(8) - X(10);
-  % h1 = validate_headway(X(10) - X(2), h_stop);
-  % h2 = validate_headway(X(2) - X(4), h_stop);
-  % h3 = validate_headway(X(4) - X(6), h_stop);
-  % h4 = validate_headway(X(6) - X(8), h_stop);
-  % h5 = validate_headway(X(8) - X(10), h_stop);
+
+  h1 = X(1);
+  h2 = X(3);
+  h3 = X(5);
+  h4 = X(7);
+  h5 = X(9);
+
   u1 = range_policy(h1, h_stop, h_go, v_max);
   u2 = range_policy(h2, h_stop, h_go, v_max);
   u3 = range_policy(h3, h_stop, h_go, v_max);
@@ -70,11 +76,28 @@ for i=1:n;
   % u=4;%fixed input
   dx=A*X+B*U;
   X=X+dx*dt;
+  % To prevent collisions between vehicles
+  [h1, vel1] = validate_headway(h1, prev_h1, h_stop, X(2), dt);
+  X(1) = h1; X(2) = vel1;
+  [h2, vel2] = validate_headway(h2, prev_h2, h_stop, X(4), dt);
+  X(3) = h2; X(4) = vel2;
+  [h3, vel3] = validate_headway(h3, prev_h3, h_stop, X(6), dt);
+  X(5) = h3; X(6) = vel3;
+  [h4, vel4] = validate_headway(h4, prev_h4, h_stop, X(8), dt);
+  X(7) = h4; X(8) = vel4;
+  [h5, vel5] = validate_headway(h5, prev_h5, h_stop, X(10), dt);
+  X(9) = h5; X(10) = vel5;
   % Y=C*X;
   % Y1(i,:)=[t, Y];
   X1(i,:)=[t, X'];
   U1(i,:)=[t, U'];
   t=t+dt;
+
+  prev_h1 = X(1);
+  prev_h2 = X(3);
+  prev_h3 = X(5);
+  prev_h4 = X(7);
+  prev_h5 = X(9);
 end
 
 % Headways
