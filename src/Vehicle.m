2583f5569cc1c413b3e13a481e78cbb67e47a26c
jay ess <mentoce@gmail.com>
2018-04-27 14:23:37 -0700

    Add accel and lead_id params, add getter/setter for accel

--- src/Vehicle.m
+++ src/Vehicle.m
@@ -6,13 +6,16 @@ classdef Vehicle < handle
 %   properties (Access=public)
 %   end
   properties %(Access=private)
-  % length: length of the vehicle [m]
+  % accel:   acceleration [m/s^2]
+  % length:  length of the vehicle [m]
   % u:       longitudinal coordinate (arc-length) [m]
   % speed:   speed of vehicle [m/s]
+    accel
     length
     u
     speed
     id
+    lead_id
   end
 
   methods
@@ -26,6 +29,14 @@ classdef Vehicle < handle
       % obj.u = u;
       % obj.speed = speed;
       % obj.id = floor(100000*rand()+200)
+    end
+
+    function set.accel(obj, a)
+      obj.accel = a;
+    end
+
+    function a = get.accel(obj)
+      a = obj.accel;
     end
 
     function set.u(obj, u)
@@ -56,7 +67,7 @@ classdef Vehicle < handle
       obj.id = n;
     end
 
-    function n = get.length(obj)
+    function n = get.id(obj)
       n = obj.id;
     end
   end
