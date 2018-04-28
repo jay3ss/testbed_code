e1ae3bc6ba31cfe08f698eae86f64b3a70fb0f91
jay ess <mentoce@gmail.com>
2018-04-27 23:43:06 -0700

    Add functions for calculating new accelerations and speeds of vehicles

--- src/Road.m
+++ src/Road.m
@@ -18,24 +18,25 @@ classdef Road < handle
     traj_x_
     traj_y_
     speed_init_
+    dt_
   end
   methods
-    function obj = Road(road_length, init_density, vehicle, v_param, driver, d_param)
+    function obj = Road(road_length, init_density, vehicle, v_param, driver, d_param, dt)
       % Constructor
       % @param road_length = length of the road
       % @param init_density = intial density of the road [vehicle/m]
       % @param vehicle: vehicle object
-      % @param v_param:   container.Map that contains init_speed and length
-      % keys
+      % @param v_param:   container.Map that contains init_speed and length keys
       % @param driver: driver object
       % @param d_param: container.Map that contains the parameters about the
       % driver
       obj.road_length = road_length;
       obj.num_vehicles = floor(obj.road_length * init_density);
       obj.vehicles = obj.createVehicleArray(vehicle, v_param, driver, d_param);
+      obj.dt_ = dt;
     end
 
-    function accels = calcAccelerations(obj)
+    function calcAccelerations(obj)
       for i = 1:obj.num_vehicles
         this_vehicle = obj.vehicles(i);
         lead_id = this_vehicle.lead_id;
@@ -46,7 +47,32 @@ classdef Road < handle
         lead_speed = lead_vehicle.speed;
         lead_accel = lead_vehicle.accel;
 
-        this_vehicle.driver.calcAccel(s, speed, lead_speed, lead_accel)
+        accel = this_vehicle.driver.calcAccel(s, speed, lead_speed, lead_accel);
+        this_vehicle.accel = accel;
+        obj.vehicles(i) = this_vehicle;
+        % obj.vehicles(i).driver.calcAccel(s, speed, lead_speed, lead_accel);
+      end
+    end
+
+    function updateSpeedPositions(obj)
+      dt = obj.dt_;
+      road_len = obj.road_length;
+
+      for i = 1:obj.num_vehicles
+        % vehicle = obj.vehicles(i);
+        speed = obj.vehicles(i).speed;
+        accel = obj.vehicles(i).accel;
+        % disp('id'); disp(obj.vehicles(i).id);
+        % disp('speed'); disp(obj.vehicles(i).speed);
+        % disp('accel'); disp(obj.vehicles(i).accel);
+        % Positional update with old speed
+        obj.vehicles(i).u = obj.vehicles(i).u + max(0, speed*dt + 0.5*accel*dt^2) - road_len;
+        % disp('u'); disp(obj.vehicles(i).u)
+
+        % Update the speed
+        obj.vehicles(i).speed = max(speed + accel*dt, 0);
+
+        % obj.vehicles(i) = vehicle;
       end
     end
 
@@ -63,25 +89,36 @@ classdef Road < handle
     end
   end
   methods (Access=private)
-    function [vehicles] = createVehicleArray(obj, vehicle, v_param)
+    function [vehicles] = createVehicleArray(obj, vehicle, v_param, driver, d_param)
       % Creates an array of homogeneous vehicles
       % @param vehicle: vehicle object
       % @param v_param:   container.Map that contains init_speed and length keys
+      % @param driver: driver object
+      % @param d_param: container.Map that contains the parameters about the
+      % driver
       % @returns vehicles: array of homogeneous vehicles
       speed = 0.8 * v_param('init_speed');
       vel_len = v_param('length');
       n_vehicles = obj.num_vehicles;
       vehicles(1, n_vehicles) = vehicle;
+      driver.a = d_param('a');
+      driver.b = d_param('b');
+      driver.s0 = d_param('s0');
+      driver.T = d_param('T');
+      driver.v0 = d_param('v0');
       % obj.vehicles(1, num_vehicles) = Vehicle(vel_length);
 
-      for i = n_vehicles:-1:1 % counting backwards for Memory pre-allocation
+      for i = 1:n_vehicles % counting backwards for Memory pre-allocation
         % find the position of the current vehicle
         u = (n_vehicles-i-1) * obj.road_length/(n_vehicles);
         vehicles(i).u = u;
         vehicles(i).length = vel_len;
         vehicles(i).speed = speed;
+        vehicles(i).driver = driver;
         if i == 1
           vehicles(i).lead_id =  n_vehicles;
+        elseif i == n_vehicles
+          vehicles(i).lead_id = 1;
         else
           vehicles(i).lead_id = i + 1;
         end
